0a82794e752169a5e2940ecdd9fb5c5cedc0bdb9
cXp1r <2510037056@qq.com>
2026-05-27 07:38:32 +0800

    feat: 新增获取用户目录函数

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -70,6 +70,12 @@ fn get_workspace() -> String {
         .to_string_lossy()
         .to_string()   
 }
+#[tauri::command]
+fn get_user_dir() -> String {
+     std::env::var("USERPROFILE")
+        .expect("USERPROFILE not set")
+        .to_string()
+}
 
 /// 全局复用的 HTTP client，天气处调用
 pub(crate) fn shared_http_client() -> &'static reqwest::blocking::Client {
@@ -273,10 +279,10 @@ pub fn run() {
             sadb::sadb_send_keycode, sadb::sadb_inject_text,
             sadb::sadb_set_clipboard,
             sadb::sadb_connect_device, sadb::sadb_disconnect_device,
-            get_workspace,
+            get_workspace, get_user_dir,
             tools::tools_download_and_install_from_github, tools::find_path_by_where,//两个通用函数
-            tools::check, tools::test,
-            tools::tools_kill_adb_server,
+            tools::check, tools::test, tools::open_dir,
+            tools::tools_kill_adb_server, tools::aria2c_download,
             tools::tools_download_and_install_adb,
             email::is_email_configured, email::fetch_emails, email::refresh_emails, email::get_email_cache_dir, email::diagnose_email_cache, email::clear_email_cache,
             email::fetch_email_uid_list, email::fetch_email_metas_by_uids, email::fetch_email_bodies_by_uids, email::fetch_email_metas_and_bodies_by_uids, email::fetch_email_body_by_uid, email::read_email_body_by_uid,
