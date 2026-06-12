3607498529a57949f77f3696373ba5fef8b2ce44
cXp1r <2510037056@qq.com>
2026-06-12 14:13:40 +0800

    feat: 添加接受adb信息的结构

--- src/ts/index/types.ts
+++ src/ts/index/types.ts
@@ -1,42 +1,11 @@
 export type ViewMode = "time" | "lyric" | "agent" | "search" | "sadb" | "email" | "downloader";
-
-
-
 export type PrivacyUsagePayload = {
-
-
-
   microphone: boolean;
-
-
-
   camera: boolean;
-
-
-
 };
-
-
-
-export type WeatherResult = {
-
-
-
-  desc: string;
-
-
-
-  temp: number;
-
-
-
-  city: string;
-
-
-
-};
-
+export type WeatherResult = {  desc: string;  temp: number;  city: string;};
 export type ClipboardUrlsPayload = {
   urls: string[];
   downloadables: boolean[];
 };
+
