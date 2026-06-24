3292bfeb8a6ed2f4e328e386437a06a80e1de9b9
cXp1r <2510037056@qq.com>
2026-06-24 21:18:39 +0800

    refactor: 初步制定挂载和初始化顺序

--- src/ts/index/main.ts
+++ src/ts/index/main.ts
@@ -1,3 +1,9 @@
+import { invoke } from "@tauri-apps/api/core";
 import { initPageRenders } from "./renders/pages";
+import { initPageStateMachine } from "./states";
+import { initComponents } from "./components/pages";
 
 initPageRenders();
+initPageStateMachine();
+initComponents();
+invoke('set_capsule_current_rect', {width: 140, height: 50})
