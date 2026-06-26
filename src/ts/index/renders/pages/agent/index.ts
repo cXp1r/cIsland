6966d743c7349a6debf23c3fd6b67b0dc027294e
cXp1r <2510037056@qq.com>
2026-06-27 01:47:30 +0800

    refactor: 初步记录几个分页的待渲染大小

---
+++ src/ts/index/renders/pages/agent/index.ts
@@ -0,0 +1,12 @@
+import { sc } from "..";
+
+export const musicList: Record<string, sc> = {
+  "collapsed": {
+    classList: [""],
+    size: [140, 50],
+  },
+  "expanded": {
+    classList: ["agent-expanded"],
+    size: [380, 420],
+  },
+};
---
+++ src/ts/index/renders/pages/downloader/index.ts
@@ -0,0 +1,12 @@
+import { sc } from "..";
+
+export const musicList: Record<string, sc> = {
+  "collapsed": {
+    classList: [""],
+    size: [380, 50],
+  },
+  "expanded": {
+    classList: ["expanded"],
+    size: [400, 300],
+  },
+};
