0c73e1131a1eb2c84b0ded09a0bc8020fc8582a1
cXp1r <2510037056@qq.com>
2026-06-28 01:32:51 +0800

    chore: 移除旧的子状态机注册

--- src/ts/index/states/pages/page-substates/registry.ts
+++ src/ts/index/states/pages/page-substates/registry.ts
@@ -1,16 +1,1 @@
-import { PageState } from "../page";
-import { timePageSubstateMachine } from "./time";
-import { lyricPageSubstateMachine } from "./music";
-import { agentPageSubstateMachine } from "./agent";
-import { sadbPageSubstateMachine } from "./sadb";
-import { emailPageSubstateMachine } from "./email";
-import { downloaderPageSubstateMachine } from "./downloader";
 
-export const pageSubstateRegistry = {
-  [PageState.Time]: timePageSubstateMachine,
-  [PageState.Music]: lyricPageSubstateMachine,
-  [PageState.Agent]: agentPageSubstateMachine,
-  [PageState.Sadb]: sadbPageSubstateMachine,
-  [PageState.Email]: emailPageSubstateMachine,
-  [PageState.Downloader]: downloaderPageSubstateMachine,
-} as const;
