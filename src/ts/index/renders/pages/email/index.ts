c8f82f43b1bf9e0e5a28d225945ac335dd30a0ff
cXp1r <2510037056@qq.com>
2026-06-27 20:39:22 +0800

    refactor: 增加邮箱的渲染表

---
+++ src/ts/index/renders/pages/email/index.ts
@@ -0,0 +1,15 @@
+import { sc } from "..";
+
+export const musicList: Record<string, sc> = {
+  "collapsed": {
+    classList: null,
+    size: [140, 50],
+  },
+  "expanded": {
+    classList: ["expanded"],
+    size: () => {
+        const style = getComputedStyle(document.documentElement);
+        return [parseInt(style.getPropertyValue('--email-view-w')), parseInt(style.getPropertyValue('--email-view-h'))]
+    },
+  },
+};
