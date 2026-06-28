abe5996d91b6f816e894bcec54045f22cdd0d593
cXp1r <2510037056@qq.com>
2026-06-28 23:56:26 +0800

    feat: 注册搜索

--- src/ts/index/components/index.ts
+++ src/ts/index/components/index.ts
@@ -1,7 +1,9 @@
 import { initDragger } from "./drag";
+import { initOverlaysC } from "./overlays";
 import { initPagesComponents } from "./pages";
 
 export function initComponents() {
     initPagesComponents()
+    initOverlaysC();
     initDragger();
 }
---
+++ src/ts/index/components/overlays/index.ts
@@ -0,0 +1,5 @@
+import { initSearchC } from "./search/search";
+
+export function initOverlaysC() {
+    initSearchC();
+}
